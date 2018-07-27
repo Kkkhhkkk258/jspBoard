6c0c985338f7127d955a8e9d266642a5b1cf144e
Kkkhhkkk258 <khyun5335@naver.com>
2018-07-27 12:36:13 +0900

    import 제거

--- src/main/java/kr/or/ddit/login/LoginServlet.java
+++ src/main/java/kr/or/ddit/login/LoginServlet.java
@@ -7,9 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import kr.or.ddit.board.service.BoardService;
-import kr.or.ddit.board.service.BoardServiceTest;
 import kr.or.ddit.board.service.BoardServiceInf;
 import kr.or.ddit.student.model.StudentVO;
 import kr.or.ddit.student.service.StudentService;
